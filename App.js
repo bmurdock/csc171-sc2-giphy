ab97ee9fa200c29b60259cafc75baeee93f61f22
bmurdock <brian.j.murdock@gmail.com>
2021-05-14 19:35:52 -0600

    new updates

--- App.js
+++ App.js
@@ -6,7 +6,13 @@ let apiRoute = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}`
 
 // These are the prebuilt components we are getting
 // from react native
-import { StyleSheet, Text, View, TextInput, Button, Image } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Image, TouchableHighlight } from 'react-native';
+
+const animals = ['cat', 'fish', 'dog', 'horse', 'emu', 'pig'];
+const getRando = (lb, ub) =>
+{
+  return Math.floor(Math.random() * (ub + 1)) + lb;
+}
 
 export default class App extends React.Component {
   constructor()
@@ -18,7 +24,28 @@ export default class App extends React.Component {
         width: 50,
         height: 50,
       },
-    }
+      whatever: 'Some String',
+    };
+  }
+
+  changeWhatever = () =>
+  {
+    this.setState({
+      whatever: "Something New",
+    });
+  }
+  // method accepts a number which
+  // represents width and height of our
+  // square image
+  imageSize = (dim) =>
+  {
+    console.log('resizing to: ', dim);
+    this.setState({
+      imgStyle: {
+        width: dim,
+        height: dim,
+      },
+    });
   }
 
   componentDidMount()
@@ -35,10 +62,6 @@ export default class App extends React.Component {
 
       this.setState({
         uri: obj.images.fixed_width.url,
-        imgStyle: {
-          width: 200,
-          height: 200,
-        },
       });
     })
     .catch((err) =>
@@ -55,10 +78,78 @@ export default class App extends React.Component {
           style={this.state.imgStyle}
           source={{uri: this.state.uri}}
         />
+        <Test
+          label="Test 1"
+          resize={this.imageSize}
+          imgStyle={this.state.imgStyle}
+          whatever={this.state.whatever}
+        />
+                <Test
+          label="Test 2"
+          resize={this.imageSize}
+          imgStyle={this.state.imgStyle}
+          changeWhatever={this.changeWhatever}
+        />
+                <Test
+          label="Test 3"
+          resize={this.imageSize}
+          imgStyle={this.state.imgStyle}
+        />
+                <Test
+          label="Not Test 4...."
+          resize={this.imageSize}
+          imgStyle={this.state.imgStyle}
+        />
         <StatusBar style="auto" />
       </View>
+
     );
   }
+}
+
+const Test = (props) =>
+{
+
+  // create a state value called animal
+  // have a function called setAnimal that changes the value of animal
+  // the initial value of animal is the first element of my global
+  // animals array
+  const [animal, setAnimal] = React.useState(animals[0]);
+
+  const refresh = () =>
+  {
+    // i want to set animal to a random animal from my 
+    // global animals array
+    setAnimal(animals[getRando(0, animals.length -1)]);
+    console.log(typeof props.whatever);
+    if (typeof props.changeWhatever !== 'undefined')
+    {
+      props.changeWhatever();
+    }
+
+  }
+
+  console.log('image style: ', props.imgStyle);
+  return (
+    <View>
+      <Text>{props.label}</Text>
+      <Text>Animal: {animal}</Text>
+      <Text>{props.whatever}</Text>
+      <MyButton label="Press Me" onPress={refresh} />
+    </View>
+  )
+}
+
+const MyButton = (props) =>
+{
+  return(
+    <TouchableHighlight 
+      style={styles.myButton}
+      onPress={props.onPress}
+    >
+      <Text>{props.label}</Text>
+    </TouchableHighlight>
+  )
 }
 
 
@@ -69,6 +160,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  myButton: {
+    alignItems: 'center',
+    padding: 5,
+    borderRadius: 5,
+    borderWidth: 1,
+  },
   person: {
     borderWidth: 1,
 
@@ -76,5 +173,13 @@ const styles = StyleSheet.create({
   tinyLogo: {
     width: 50,
     height: 50,
+  },
+  small: {
+    width: 60,
+    height: 60,
+  },
+  medium: {
+    width: 150,
+    height: 150,
   },
 });
---
+++ src/Giphy.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, Button, Image, TouchableHighlight } from 'react-native';
+
+const Giphy = (props) =>
+{
+    return (
+        <View></View>
+    )
+}
